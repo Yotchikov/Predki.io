f2b07b365a9c3f0377850198854d1496493a3e7b
Anton <books.antony@gmail.com>
2020-03-13 19:03:48 +0300

    working with add component

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -49,8 +49,63 @@ export const Add = ({ history }) => {
             />
           </div>
         </div>
-        <SelectDate />
-
+        <div className="form-row">
+          <div className="col-6 form-group">
+            <label htmlFor="inputDate">Дата рождения</label>
+            <SelectDate />
+          </div>
+          <div className="col-6 form-group">
+            <label htmlFor="inputDate">Дата смерти *</label>
+            <SelectDate />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="col-7 form-group">
+            <label htmlFor="nativeCity">Место рождения</label>
+            <input
+              type="text"
+              className="form-control"
+              id="nativeCity"
+              placeholder="Город"
+            />
+          </div>
+          <div className="col-4 form-group">
+            <label htmlFor="">Фотография</label>
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="customFile" lang="ru" />
+              <label class="custom-file-label" for="customFile">
+                Выбрать файл
+              </label>
+            </div>
+          </div>
+          <div className="col-1 form-group">
+            <label htmlFor="sex">Пол</label>
+            <ul className="sex-choose">
+              <li>
+                <input
+                  type="radio"
+                  value="Мужской"
+                  name="sex"
+                  id="male"
+                  defaultChecked
+                />
+                <label className="form-check-label" htmlFor="male">
+                  <i className="fa fa-mars" aria-hidden="true"></i>
+                </label>
+              </li>
+              <li>
+                <input type="radio" value="Женский" name="sex" id="female" />
+                <label className="form-check-label" htmlFor="female">
+                  <i className="fa fa-venus" aria-hidden="true"></i>
+                </label>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Биография</label>
+          <textarea className="form-control"></textarea>
+        </div>
       </form>
     </div>
   );
